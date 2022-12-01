88ae5441d9266c1dab4cff6040d75ea510ac9920
lucymell <lucy.mutuota@gmail.com>
2022-12-01 16:10:42 +0300

    added project files

---
+++ README.md
@@ -0,0 +1,109 @@
+Title: Music Genre Detection and Recommendation.
+
+Authors: Lucy Mutuota
+
+
+# Music Genre Detection and Recommendation.
+
+## About the Project:
+Music is an essential unwinding for most of us. With new songs added to music streaming platforms like Spotify, Amazon Prime Music, JioSaavn every week, music listeners want to keep their playlist updated. With this project we aim to provide a similar experience with regards to music recommendation for those who use the aforementioned applications and to those who don’t. 
+The aim of this project is to classify the genre of songs, mainly into: Pop, Rock, Hip-Hop, Reggae, Country, Disco, Classical, Jazz with the help of an application built on Flask. Along with the genre, users of the application get recommended with upto three songs similar to the one searched for.
+Key features:
+- Easy to use interface
+- Smaller application size
+- Accurate genre predictions
+- Recommendations to help build your playlist for an enthralling musical experience
+
+### Disclaimer:
+- The app supports the use of .WAV file format. Please use the link in the application to convert to .WAV format from .MP3 or .MPEG formats.
+
+### Dependencies:
+We use the following python libraries:
+* [numpy]
+* [os]
+* [sklearn]
+* [pandas]
+* [librosa]
+* [pickle]
+* [tensorflow]
+* [statistics]
+* [math]
+* [keras]
+* [flask]
+
+- Install The Dependencies By Running
+```!pip install -r requirements.txt```
+
+### Run the application:
+
+- Download the application files from this repo as a zip file
+
+- Unzip the contents
+
+- Open the Command prompt/ Terminal/ Windows Powershell and cd to the folder’s directory
+
+- Ensure python v.3 is running on your system.
+
+- For windows:
+
+— - Type the following in : Command prompt/ Windows Powershell
+
+- --
+
+```sh
+set FLASK_APP = app.py
+```
+— -- 
+
+ ```sh
+set FLASK_DEBUG = 1
+```
+— -- 
+
+```sh
+flask run
+```
+— -- 
+
+— Type the ‘localhost’ url displayed on your command line prompt in your browser.
+
+- For MacOS/ Linux:
+— - Type the following in Terminal”
+
+```sh
+ python -3 ./app.py
+ ```
+—Type the ‘localhost’ url displayed on your terminal in your browser.
+
+### Working Demo
+
+- The website will look as shown below. Press the red button to upload a song file of your choice in .wav formt. If you have the music file in any other format, there is a link provided below the button to convert the any type of audio file to a .wav file.
+![Input Image](https://github.com/lucymell/Zene_v4/blob/main/Output/input.png)
+
+- After processing (which might take upto 30 seconds depending on the file size) you will receive the output as follows. The website will tell you a genre for your uploaded file and on the basis of that recommend you songs of the same genre. The list of all songs available for recommendations along with their genre is given in the Models folder as a .csv file.
+![Output Image](https://github.com/lucymell/Zene_v4/blob/main/Output/output.png)
+
+### Development
+
+Developed By :-
+[Lucy Mutuota]
+
+
+[//]: #
+	[numPy]: <https://numpy.org>
+	[pandas]: <https://pandas.pydata.org>
+	[sklearn]: <https://scikit-learn.org/stable/>
+	[os]: <https://docs.python.org/3/library/os.html>
+	[librosa]: <https://pandas.pydata.org>
+	[pickle]: <https://docs.python.org/3/library/pickle.html>
+	[os]: <https://docs.python.org/3/library/os.html>
+	[librosa]: <https://librosa.org/doc/latest/index.html>
+	[tensorflow]: <https://www.tensorflow.org>
+	[statistics]: <https://docs.python.org/3/library/statistics.html>
+	[math]: <https://docs.python.org/3/library/math.html>
+	[keras]: <https://keras.io>
+	[flask]: <https://flask.palletsprojects.com/en/1.1.x/>
+	[Arth Akhouri]: <https://github.com/user/FlintyTub49>
+	[Mithesh R]: <https://github.com/user/259-mit>
+	[Heetansh Jhaveri]: <https://github.com/user/hjj31>
+	[Ayaan Khan]: <https://github.com/user/ayaan-27>
---
+++ app.py
@@ -0,0 +1,123 @@
+from flask import Flask, render_template, request
+import numpy as np
+import librosa
+import pandas as pd
+import os
+import pickle as pk
+from tensorflow.keras.models import load_model
+import math
+from statistics import mode
+
+app = Flask(__name__)
+app.config['UPLOADS'] = 'uploads'
+
+
+# --------------------------------------------------
+# Loading The Model, Label Encoder & Recommendations
+# --------------------------------------------------
+codePath = os.path.dirname(os.path.abspath('app.py'))
+le = os.path.join(codePath, 'Models/le.pk')
+cnn = os.path.join(codePath, 'Models/best_model.h5')
+recom = os.path.join(codePath, 'Models/Final Recs.csv')
+
+le = pk.load(open(le, 'rb'))
+model = load_model(cnn)
+recs = pd.read_csv(recom)
+
+# -------------------------------------
+# Render Main Home Template Index.html
+# -------------------------------------
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+# --------------------------------------
+# Parameters to Preprocess Data
+# --------------------------------------
+SAMPLE_RATE = 22050
+TRACK_DURATION = 30
+SAMPLES_PER_TRACK = SAMPLE_RATE * TRACK_DURATION
+num_segments = 10
+num_mfcc = 13
+n_fft = 2048
+hop_length = 512
+
+
+# --------------------------------------
+# Preprocesses User Input Taking a File
+# --------------------------------------
+def getUserInput(path, genre):
+  samples_per_segment = int(SAMPLES_PER_TRACK / num_segments)
+  num_mfcc_vectors_per_segment = math.ceil(samples_per_segment / hop_length)
+
+  user = {"labels": [], "mfcc": []}
+
+  signal, sample_rate = librosa.load(path, sr = SAMPLE_RATE)
+
+  # process all segments of audio file
+  for d in range(num_segments):
+
+      # calculate start and finish sample for current segment
+      start = samples_per_segment * d
+      finish = start + samples_per_segment
+
+      # # extract mfcc
+      if len(signal[start : finish]) == samples_per_segment:
+        mfcc = librosa.feature.mfcc(signal[start:finish], sample_rate, n_mfcc = num_mfcc, 
+                                    n_fft = n_fft, hop_length = hop_length)
+        mfcc = mfcc.T
+
+        # # store only mfcc feature with expected number of vectors
+        if len(mfcc) == num_mfcc_vectors_per_segment:
+            user["mfcc"].append(mfcc.tolist())
+            user["labels"].append(genre)
+
+  x_user = np.array(user['mfcc'])
+  y_user = np.array(user['labels'])
+  return x_user, y_user
+
+
+# --------------------------------------
+# Main Function To Process Data and Display Output
+# --------------------------------------
+@app.route('/', methods = ['POST'])
+def upload_files():
+
+    # ---------------------------- Get File From User ---------------------------- #
+    file = request.files['audiofile']
+    filepath = os.path.join(app.config['UPLOADS'], file.filename)
+    file.save(filepath)
+
+
+    # ------------------- Preprocess User Input To Put in Model ------------------ #
+    x_user, y_user = getUserInput(filepath, 'rock')
+    x_user = x_user[..., np.newaxis]
+    
+    
+    # ----------------------------- Running The Model ---------------------------- #
+    pred = np.argmax(model.predict(x_user), axis = -1)
+    genre = le.inverse_transform([mode(pred)])[0]
+    
+    os.unlink(filepath)
+
+
+    # ------------------------ Getting The Recommendation ------------------------ #
+    recommend = recs[recs['Genre'] == genre]
+
+    if recommend.shape[0] >= 3: sample = 3
+    else: sample = recommend.shape[0]
+    # print('\nSong Recommendations For You Are:')
+    df = recommend.sample(sample)
+
+
+    # ------------------ Printing The Genre With Recommendations ----------------- #
+    genre = 'Your Predicted Genre is {}'.format(genre)
+    dummy = df.to_html(classes = 'table-data')
+    return render_template('index.html', label = genre, 
+                           tables=[dummy], titles=df.columns.values)
+    # return render_template('index.html', label = genre)
+
+if __name__ == '__main__':
+    # bestModel()
+    app.run(debug = True)
